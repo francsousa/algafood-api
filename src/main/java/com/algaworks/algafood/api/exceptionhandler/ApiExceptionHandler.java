c18aa0f1b9f8144859aaefd8665fe3b9a38f6dae
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-04-13 16:54:18 -0300

    Implementando exception handler

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 
@@ -37,6 +38,17 @@ public class ApiExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 				.body(problema);
+	}
+	
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
+	    Problema problema = Problema.builder()
+	            .dataHora(LocalDateTime.now())
+	            .mensagem(e.getMessage())
+	            .build();
+	    
+	    return ResponseEntity.status(HttpStatus.CONFLICT)
+	            .body(problema);
 	}
 	
 	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
