66d3f22524d1bfc59a80db3f0c0897dcb1e0266f
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-04-22 08:13:49 -0300

    Ajustando Exception Handler para adicionar mensagens de validação em nível de classe

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -207,13 +208,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 		BindingResult bindingResult = ex.getBindingResult();
 
-		List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream().map(fieldError -> {
-			String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+		List<Problem.Object> problemObjects = bindingResult.getAllErrors().stream().map(objectError -> {
+			String message = messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
+			
+			String name = objectError.getObjectName();
+			
+			if (objectError instanceof FieldError) {
+				name = ((FieldError) objectError).getField();
+			}
 
-			return Problem.Field.builder().name(fieldError.getField()).userMessage(message).build();
+			return Problem.Object.builder().name(name).userMessage(message).build();
 		}).collect(Collectors.toList());
 
-		Problem problem = createProblemBuilder(status, problemType, detail).fields(problemFields).userMessage(detail)
+		Problem problem = createProblemBuilder(status, problemType, detail).objects(problemObjects).userMessage(detail)
 				.build();
 
 		return handleExceptionInternal(ex, problem, headers, status, request);
