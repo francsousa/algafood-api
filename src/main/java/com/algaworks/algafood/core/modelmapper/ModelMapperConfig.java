7cd059afc36d2313b35614bdf07b22699c9128c2
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-04-29 07:52:10 -0300

    Mapeando para uma instância destino (e não um tipo) com ModelMapper

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,9 +4,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.algaworks.algafood.api.model.RestauranteModel;
-import com.algaworks.algafood.domain.model.Restaurante;
-
 @Configuration
 public class ModelMapperConfig {
 	
@@ -14,8 +11,8 @@ public class ModelMapperConfig {
 	public ModelMapper modelMapper() {
 		var modelMapper = new ModelMapper();
 		
-		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
-			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+//		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+//			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
 		
 		return modelMapper;
 	}
