7ac829a84d6bb39eac37e8722d1b6e67b3e910ec
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-05-20 08:13:52 -0300

    Implementando consulta com dados agregados de vendas diárias

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -27,13 +28,12 @@ import com.algaworks.algafood.api.model.input.PedidoInput;
 import com.algaworks.algafood.core.data.PageableTranslator;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.filter.PedidoFilter;
 import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.repository.PedidoRepository;
-import com.algaworks.algafood.domain.repository.filter.PedidoFilter;
 import com.algaworks.algafood.domain.service.EmissaoPedidoService;
 import com.algaworks.algafood.infrastructure.repository.spec.PedidoSpecs;
-import com.google.common.collect.ImmutableMap;
 
 @RestController
 @RequestMapping(value = "/pedidos")
@@ -95,11 +95,16 @@ public class PedidoController {
     }
     
     private Pageable traduzirPageable(Pageable apiPageable) {
-    	var mapeamento = ImmutableMap.of(
+    	var mapeamento = Map.of(
     			"codigo", "codigo",
-    			"restaurante.nome", "restaurante.nome",
-    			"nomeCliente", "cliente.nome",
-    			"valorTotal", "valorTotal"
+				"subtotal", "subtotal",
+				"taxaFrete", "taxaFrete",
+				"valorTotal", "valorTotal",
+				"dataCriacao", "dataCriacao",
+				"restaurante.nome", "restaurante.nome",
+				"restaurante.id", "restaurante.id",
+				"cliente.id", "cliente.id",
+				"cliente.nome", "cliente.nome"
     		);
     	
     	return PageableTranslator.translate(apiPageable, mapeamento);
